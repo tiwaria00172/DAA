2fc1c5e0aeccb22f255436c1a97e2ee6fea08ab3
tiwaria00172 <tiwariabhishek00172@gmail.com>
2026-04-09 17:11:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1121,4 +1121,5 @@ if __name__ == '__main__':
     ║  Press Ctrl+C to stop the server                         ║
     ╚══════════════════════════════════════════════════════════╝
     """)
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(debug=False, host='0.0.0.0', port=port)
